94200e65fa7e40f4319f87ac453b9863b80e9351
moinur-rahman <rahmanmoin27@gmail.com>
2022-10-31 12:50:28 +0600

    Project routing added

---
+++ client/src/app/developer-page/developer-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeveloperPageComponent } from './developer-page.component';
+
+describe('DeveloperPageComponent', () => {
+  let component: DeveloperPageComponent;
+  let fixture: ComponentFixture<DeveloperPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DeveloperPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DeveloperPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
